9aeaece96219be25aec38cd83ffb860044a747f6
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-03-11 16:57:52 -0600

    POST confirmed  working with user input

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,35 @@
 $(handleReady);
 
-function handleReady() { 
+function handleReady() {
     console.log('jQ ready to roll!');
     $('#submitButton').on('click', handleSubmit);
+    // $('#add').on('click', doAddition);
+    // $('#subtract').on('click', doSubtraction);
+    // $('#multiply').on('click', do);
+    // $('#divide').on('click', );
+    // $('#clearButton').on('click', );
+
+}
+
+function handleSubmit () {//POST a set of numbers to server
+    let firstNumber = $('#firstNumber').val();
+    let operator = '+'; //placeholder
+    let secondNumber = $('#secondNumber').val();
+
+
+$.ajax({
+    url: '/mathResults',
+    method: 'POST',
+    data: {
+        firstNumber: firstNumber,
+        operator: operator,
+        secondNumber: secondNumber,
+    }
+}).then(function (response) {
+    console.log('response');
+    // getGuesses();
+    $('input').val('');
+}).catch(function (error) {
+    console.log(error);
+})
 }
