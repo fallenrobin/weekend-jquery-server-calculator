f344c5f72bf337137550e06b781b8479d27e24bd
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-03-12 13:51:59 -0600

    Created click listeners and functions to respond to all the operator and clear buttons

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,17 +3,37 @@ $(handleReady);
 function handleReady() {
     console.log('jQ ready to roll!');
     $('#submitButton').on('click', handleSubmit);
-    // $('#add').on('click', doAddition);
-    // $('#subtract').on('click', doSubtraction);
-    // $('#multiply').on('click', do);
-    // $('#divide').on('click', );
-    // $('#clearButton').on('click', );
+    $('#add').on('click', setAddition);
+    $('#subtract').on('click', setSubtraction);
+    $('#multiply').on('click', setMultiplication);
+    $('#divide').on('click', setDivision);
+    $('#clearButton').on('click', clearCalculator);
 
+}
+
+let operatorClicked = '';// this is a variable to store which operator button
+//  was clicked so it goes into the object for the server
+
+function setAddition() {
+    operatorClicked = '+';
+}
+function setSubtraction() {
+    operatorClicked = '-';
+
+}function setMultiplication() {
+    operatorClicked = '*';
+
+}function setDivision() {
+    operatorClicked = '/';
+}
+
+function clearCalculator() {//clears the number inputs
+    $('input').val('');
 }
 
 function handleSubmit() {//POST a set of numbers to server
     let firstNumber = $('#firstNumber').val();
-    let operator = '+'; //placeholder
+    let operator = operatorClicked; //uses the stored 'click record'
     let secondNumber = $('#secondNumber').val();
 
 
